9a64a152201b4dcfd4b53d54054a64f542368726
WiktoriaGit <wiktoriagithub@gmail.com>
2025-01-06 21:20:05 +0100

    Dodano wczytywanie pliku csv

--- analizaEnergetyczna/main.cpp
+++ analizaEnergetyczna/main.cpp
@@ -1,11 +1,35 @@
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
 int main()
 {
 	cout << "Hello World!" << endl;
+
+    ifstream file("Chart Export.csv");
+    if (!file.is_open())
+    {
+        cerr << "Error opening file" << endl;
+        return 1;
+    }
+
+    string line;
+    while (getline(file, line))
+    {
+        stringstream ss(line);
+        string value;
+        while (getline(ss, value, ','))
+        {
+            cout << value << " ";
+        }
+        cout << endl;
+    }
+
+    file.close();
+
 
 	return 0;
 }
