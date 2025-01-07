b6c36630ae6bd76ae802cccc5fd1f9ed0bf01ce8
Mokkaq <gbugajska051@gmail.com>
2025-01-08 00:35:25 +0100

    Logger.h doxy

--- analizaEnergetyczna/Logger.h
+++ analizaEnergetyczna/Logger.h
@@ -1,24 +1,43 @@
+/// \file Logger.h
+/// \brief Deklaracja klasy Logger do obs³ugi logowania komunikatów.
+
 #ifndef LOGGER_H
 #define LOGGER_H
 
 #include <fstream>
 #include <string>
 
+/// \class Logger
+/// \brief Klasa obs³uguj¹ca logowanie komunikatów do plików tekstowych.
 class Logger {
 public:
+    /// \brief Konstruktor klasy Logger.
+    /// \details Tworzy plik logu z unikaln¹ nazw¹ opart¹ na aktualnej dacie i godzinie.
+    /// \param filename Nazwa podstawowa pliku logu.
     Logger(const std::string& filename);
+
+    /// \brief Destruktor klasy Logger.
+    /// \details Zamyka otwarty plik logu.
     ~Logger();
+
+    /// \brief Zapisuje komunikat do pliku logu.
+    /// \param message Komunikat do zapisania.
     void log(const std::string& message);
 
 private:
-    std::ofstream logFile;
+    std::ofstream logFile; ///< Strumieñ pliku logu.
 };
 
-// Declare global logger instances
+/// \var logger
+/// \brief Globalny logger dla standardowych komunikatów.
 extern Logger logger;
+
+/// \var loggerError
+/// \brief Globalny logger dla komunikatów b³êdów.
 extern Logger loggerError;
 
-// Declare global counter for loggerError executions
+/// \var loggerErrorCount
+/// \brief Licznik wyst¹pieñ b³êdów logowanych przez loggerError.
 extern int loggerErrorCount;
 
 #endif // LOGGER_H
